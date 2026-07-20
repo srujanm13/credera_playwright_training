caf59b95f53fa55442c9a2181278866668c8387a
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 20:30:07 +0530

    Add Day1 first assignment

---
+++ keerthy_playwright/Day1/first_assignment.js
@@ -0,0 +1,8 @@
+//Create array and stored browser names
+const browserNames = ["Chrome", "Firefox", "Edge", "Opera", "Safari"];
+
+//Retrieve third browser name from array and store in a variable
+let thirdbrowserName = browserNames[2];
+
+//Print third browser name from the array
+console.log("Third Browser Name: " + thirdbrowserName);
