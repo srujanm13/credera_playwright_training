0ffb9ca2fde16e951521ac51ac7f825f2c94780f
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-07-20 19:44:15 +0530

    Day1 JavaScript assignment
    All the assignments are added here. please review and provide feedback

---
+++ Onkar_Playwright/Day1/First_Assignment.js
@@ -0,0 +1,3 @@
+//1.  Create an array of five browser names and print the third browser. 
+let Browsers = ["Google Chrome","Microsoft Edge","Safari", "Mozilla Firefox", "Internet Explorer"]
+console.log("Third Browser is " + Browsers[2]);
---
+++ Onkar_Playwright/Day1/Third_Assignment.js
@@ -0,0 +1,6 @@
+//3.Convert "99.99" to a number and add 0.01. 
+
+const int = Number("99.99")
+console.log(int)
+const a = 0.01;
+console.log("The addition of two numbers are " + (int + a))
