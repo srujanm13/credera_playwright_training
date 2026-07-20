38f670afd8a9157f56ec492a12c6803f64af9255
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 22:16:26 +0530

    Add Day1 seventh assignment

---
+++ keerthy_playwright/Day1/seventh_assignment.js
@@ -0,0 +1,9 @@
+//Prints True
+//== does loose equality check i.e. converts false value to 0 before comparing with 0 value
+//0 value stands for false and 1 value stands for true
+console.log(false == 0);
+
+//Prints false
+//=== Does strict equality check i.e. compares both the value and the datatype
+//false value is of boolean data type and 0 value is of number data type
+console.log(false === 0);
