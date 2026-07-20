7d26082cb01c6729da400c0b41225f5a79a440a9
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 22:00:39 +0530

    Add Day1 sixth assignment

--- keerthy_playwright/Day1/fifth_assignment.js
+++ keerthy_playwright/Day1/fifth_assignment.js
@@ -7,7 +7,6 @@ let age = Number(ageValue);
 //Check whether the user is an adult or not and print
 if (age >= 18) {
     console.log("The user is an adult.");
-
 } else {
     console.log("The user is not an adult.");
 }
