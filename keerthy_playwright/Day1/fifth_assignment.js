3a8a8690b8d4c3b23ef13411e7981f485d4501c5
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 21:39:18 +0530

    Add Day1 fifth assignment

---
+++ keerthy_playwright/Day1/fifth_assignment.js
@@ -0,0 +1,13 @@
+//Store age in a variable as a string
+let ageValue = "37";
+
+//Convert text to number and store in a variable
+let age = Number(ageValue);
+
+//Check whether the user is an adult or not and print
+if (age >= 18) {
+    console.log("The user is an adult.");
+
+} else {
+    console.log("The user is not an adult.");
+}
