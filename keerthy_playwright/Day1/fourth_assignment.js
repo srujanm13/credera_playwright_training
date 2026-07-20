30f9325d1728be520f093e36a4366630bc14382f
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 21:24:50 +0530

    Add Day1 fourth assignment

--- keerthy_playwright/Day1/fourth_assignment.js
+++ keerthy_playwright/Day1/fourth_assignment.js
@@ -4,7 +4,7 @@ let weightValue = "250kg";
 //Extract the number from the weight value and store it in a variable
 let weight = parseInt(weightValue);
 
-//Multiply the weight by 2 and store it in a varaiable
+//Multiply the weight by 2 and store it back in the same variable
 weight = weight * 2;
 
 //Print the weight
