8d910de89c940802e1b1b97392b7681d3b6502e3
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-20 21:01:49 +0530

    Add Day1 third assignment

---
+++ keerthy_playwright/Day1/third_assignment.js
@@ -0,0 +1,11 @@
+//Store number in a variable as string
+let numValue = "99.99";
+
+//Convert text to number and atore in a variable
+let num = Number(numValue);
+
+//Add value to the number
+num = num + 0.01;
+
+//Print the number
+console.log("Result: " + num);
