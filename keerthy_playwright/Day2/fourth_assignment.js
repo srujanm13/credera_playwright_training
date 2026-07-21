665cbe7cadb632b24f41e75a4991e46d6660e05e
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-21 22:54:49 +0530

    Add Day 2 fourth assignment

--- keerthy_playwright/Day2/fourth_assignment.js
+++ keerthy_playwright/Day2/fourth_assignment.js
@@ -1,15 +1,14 @@
-// Array containing marks of students
+//Array containing marks of students
 const marks = [75, 80, 65, 90, 88];
 
-// Variable to store the total sum of marks
+//Variable to store the total sum of marks
 let total = 0;
 
-// Loop through each mark in the array
+//Loop through each mark in the array
 for (const mark of marks) {
-
-    // Add the current mark to the total
+    //Add the current mark to the total
     total += mark;
 }
 
-// Display the total marks
-console.log("Total Marks:", total);
+//Display the total marks in the console
+console.log("Total Marks: ", total);
