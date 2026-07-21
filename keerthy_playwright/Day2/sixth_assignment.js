f1fecc65ffbe7b1d706e9f40435fddd5d3b0576b
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-21 23:07:29 +0530

    Add Day 2 sixth assignment

--- keerthy_playwright/Day2/sixth_assignment.js
+++ keerthy_playwright/Day2/sixth_assignment.js
@@ -1,11 +1,9 @@
 // Arrow function to calculate the cube of a number
 const cube = (num) => num * num * num;
 
-// Test case: Cube of 2 (2 × 2 × 2 = 8)
+// Calling the function with different numbers and logging the results
 console.log(cube(2));
 
-// Test case: Cube of 3 (3 × 3 × 3 = 27)
 console.log(cube(3));
 
-// Test case: Cube of 5 (5 × 5 × 5 = 125)
 console.log(cube(5));
