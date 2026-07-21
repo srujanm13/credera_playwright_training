fa6d21595f7608fc39e4423915f5e80b7739b786
Vamsikrishna Alasandalapalli (Omnicom) <vamsikrishna.a@omc.com>
2026-07-21 18:20:30 +0530

    add Task6

---
+++ Vamsi_playwright/Day_1/Task6.js
@@ -0,0 +1,9 @@
+
+//Create an object representing a test case (id, title, status) and display it using console.table()
+
+let testCase = {
+    id: 1,
+    title: "Login Test",
+    status: "Passed"
+};
+console.table(testCase);
