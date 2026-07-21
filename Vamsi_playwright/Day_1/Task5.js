807a430ca620fefcb88fee8ccff6c743be678b92
Vamsikrishna Alasandalapalli (Omnicom) <vamsikrishna.a@omc.com>
2026-07-21 18:02:04 +0530

    add Task5

---
+++ Vamsi_playwright/Day_1/Task5.js
@@ -0,0 +1,11 @@
+
+//Write a program that stores a user's age as a string, converts it to a number, checks whether the user is an adult, and prints an appropriate message. 
+
+let ageString = "20"; 
+let ageNumber = Number(ageString); 
+
+if (ageNumber >= 18) {
+    console.log("The user is an adult.");
+} else {
+    console.log("The user is not an adult.");
+}
