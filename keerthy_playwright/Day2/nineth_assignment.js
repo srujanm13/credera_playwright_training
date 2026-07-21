ec856b78ebcd9d190ab58e5837d93632067b0785
Keerthy Arvind <keerthyarvind@gmail.com>
2026-07-21 23:20:09 +0530

    Add Day 2 nineth assignment

--- keerthy_playwright/Day2/nineth_assignment.js
+++ keerthy_playwright/Day2/nineth_assignment.js
@@ -5,7 +5,7 @@ const browsers1 = ["Chrome", "Firefox"];
 const browsers2 = ["Edge", "Safari"];
 
 // Use the spread operator (...) to combine both arrays into a new array
-const mergedBrowsers = [...browsers1, ...browsers2];
+const browsersList = [...browsers1, ...browsers2];
 
 // Display the merged array of browsers
-console.log(mergedBrowsers);
+console.log(browsersList);
