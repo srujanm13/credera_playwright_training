b3ade2ae097c88daa3643e87bf1b03ea36feeb31
anjana0894-qa <anjana5na94@gmail.com>
2026-07-22 15:52:08 +0530

    Day 2 assignments

---
+++ anjana_playwright/Day2/assignment_2.js
@@ -0,0 +1,10 @@
+let marks = 34
+if (marks >= 85) {
+    console.log("Distinction")
+}
+else if (marks >= 35) {
+    console.log("Passed")
+}
+else {
+    console.log("Failed")
+}
---
+++ anjana_playwright/Day2/assignment_4.js
@@ -0,0 +1,6 @@
+const marks = [75, 80, 65, 90, 88]
+let total = 0
+for (const i of marks) {
+    total +=i
+}
+console.log("Total Marks ", total)
