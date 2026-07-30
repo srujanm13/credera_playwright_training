8edd09f053826e85a1f83a81b3fdeccb266a3457
Vijaya <vijaya.salveru@wnco.com>
2026-07-30 06:48:36 -0500

    day2

---
+++ tests/helpers/smarterp.js
@@ -0,0 +1,19 @@
+import {expect} from '@playwright/test'
+
+export async function loginToSmartERP(page) {
+    await page.goto("https://smarterp-wgaw.onrender.com/", {waitUntil:"domcontentloaded"});
+    await page.locator("#username").fill("admin");
+    await page.locator("#password").fill("admin123");
+    await page.locator("#loginBtn").click();
+    await expect(page).toHaveURL(/dashboard\.html$/);
+}
+
+export async function openPlayground(page) {
+    await page.getByRole("link",{name:/Playground/}).click();
+    await expect(page).toHaveURL(/playground\.html$/);
+}
+
+export async function openCustomersPage(page) {
+    await page.locator("#menuCustomers").click();
+    await expect(page.locator("#customersTable")).toBeVisible();
+}
