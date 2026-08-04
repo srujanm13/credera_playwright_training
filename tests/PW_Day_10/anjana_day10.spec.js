ababcfca93dbc4699da5e4e6528a7b0dea342bc9
anjana0894-qa <anjana5na94@gmail.com>
2026-08-04 14:44:17 +0530

    fix frames script - assignment 10

--- tests/PW_Day_10/anjana_day10.spec.js
+++ tests/PW_Day_10/anjana_day10.spec.js
@@ -3,33 +3,33 @@ import { test, expect } from "@playwright/test";
 test("Handle multiple frames in Oracle Java Docs", async ({ page }) => {
   await page.goto(
     "https://docs.oracle.com/javase/8/docs/api/index.html?overview-summary.html",
-    { waitUntil: "domcontentloaded" }
+    { waitUntil: "domcontentloaded" },
   );
-  console.log("Total Frames:", page.frames().length)
-  expect(page.frames().length).toBe(0);
+  console.log("Total Frames:", page.frames().length);
+  expect(page.frames().length).not.toBe(0);
   const packageFrame = page.frameLocator('frame[name="packageListFrame"]');
   await packageFrame.getByText("java.applet").click();
   const classFrame = page.frameLocator('frame[name="packageFrame"]');
-  await classFrame.getByText("Applet").click();
+  await classFrame.getByRole("link", { name: "Applet", exact: true }).click();
   const classDocFrame = page.frameLocator('frame[name="classFrame"]');
   await expect(
-    classDocFrame.getByRole("heading", { name: "Applet" })
+    classDocFrame.getByRole("heading", {name: "Applet",exact: true,}),
   ).toBeVisible();
   await expect(classDocFrame.locator("body")).toContainText("Applet");
   console.log("Successfully navigated through all frames.");
 });
 
-test.only("Handling Multiple Tabs", async ({ page, context }) => {
+test("Handling Multiple Tabs", async ({ page, context }) => {
   await page.goto("https://www.hyundai.com/in/en", {
     waitUntil: "domcontentloaded",
   });
-   // Open new tab
+  // Open new tab
   const [newPage] = await Promise.all([
     context.waitForEvent("page"),
     page.locator('a[aria-label="testdrive"]').click(),
   ]);
 
-  // Enter name only 
+  // Enter name only
   const nameField = newPage.locator('input[placeholder="Name"]');
   const expectedName = "John Doe";
   await nameField.fill(expectedName);
