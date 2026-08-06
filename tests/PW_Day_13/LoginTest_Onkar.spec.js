44574c9ac7d704956a8fc6611a400952d42e7054
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-08-06 20:17:17 +0530

    Day 12 and 13 Assignments - POM

---
+++ tests/PW_Day_13/LoginTest_Onkar.spec.js
@@ -0,0 +1,16 @@
+import {test,expect} from "../../Fixtures/LoginFixture_Onkar.js";
+
+test.describe("SMARTERP Login Test",() => {
+  test("Verify trainer Login",async ({ loginPage }) => {
+        await loginPage.launchSmartERP();
+        await loginPage.verifyLoginPageLoaded();
+        await loginPage.loginAsTrainer();
+        await loginPage.verifyLoginSuccessful();
+    });
+    test("Verify Admin Login",async ({ loginPage }) => {
+        await loginPage.launchSmartERP();
+        await loginPage.verifyLoginPageLoaded();
+        await loginPage.loginAsAdmin();
+        await loginPage.verifyLoginSuccessful();
+    });
+});
