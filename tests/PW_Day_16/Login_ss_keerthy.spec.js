d9213d21861e4fbeba6fe74d2d97e04b42584871
Keerthy Arvind <keerthyarvind@gmail.com>
2026-08-12 09:16:04 +0530

    Added Day14 & Day16

---
+++ tests/PW_Day_16/Login_ss_keerthy.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+test.describe.configure({ mode: 'serial' });
+test('Login test', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#user-name').fill('standard_user');
+    await page.locator('#password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await expect(page.getByText('Products')).toBeVisible();
+    await page.context().storageState({
+        path: 'storageState.json'
+    });
+});
+test.use({
+    storageState: './storageState.json'
+});
+
+test('Verify Products', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/inventory.html');
+    await expect(page.getByText('Products')).toBeVisible();
+});
