94e9b165b4b866c464efa0e1150f9b156f239bd7
sankalpgit97-design <sankalpgit97@gmail.com>
2026-08-25 16:42:19 +0530

    assignment till day 14

---
+++ Pages/BasePage_Sankalp.js
@@ -0,0 +1,30 @@
+export class BasePage {
+    
+    constructor(page) {
+        this.page = page;
+    }
+    async navigate(url) {
+        await this.page.goto(url, {
+            waitUntil: "domcontentloaded"
+        });
+    }
+    async enterText(locator, text) {
+        await locator.fill(text);
+    }
+
+    async clickElement(locator) {
+        await locator.click();
+    }
+
+    async getPageTitle() {
+        return await this.page.title();
+    }
+    async getCurrentUrl() {
+        return this.page.url();
+    }
+    async waitForElement(locator) {
+        await locator.waitFor({
+            state: "visible"
+        });
+    }
+}
